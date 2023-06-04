73c76c283240ab24971e7fd0901224b1e99ae025
clara-schiappacassi <claraschiappacassi@gmail.com>
2023-06-04 11:01:42 -0300

    ver1

---
+++ javascript/script_nav.js
@@ -0,0 +1,27 @@
+
+//  seleccionamos los dos elementos que serán clickables
+
+const toggleButton = document.getElementById("_toggle");
+const navWrapper = document.getElementById("_items");
+/* 
+  cada ves que se haga click en el botón 
+  agrega y quita las clases necesarias 
+  para que el menú se muestre.
+*/
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("close");
+  navWrapper.classList.toggle("show");
+});
+
+/* 
+  Cuándo se haga click fuera del contenedor de enlaces 
+  el menú debe esconderse.
+*/
+
+navWrapper.addEventListener("click", e => {
+  if (e.target.id === "_items") {
+    navWrapper.classList.remove("show");
+    toggleButton.classList.remove("close");
+  }
+});
+   
